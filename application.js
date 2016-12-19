5d3b66cf8f1fd29d1a69d1db5dd3f1980d73db47
ConsenHuang <810041193@qq.com>
2016-12-19 21:34:32 +0800

    bnhj

--- application.js
+++ application.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-app.use( '/www', express.static('./www') );
+app.use( '/www', express.static('./') );
 app.listen(8888, function() {
     console.log('服务器开启成功');
     console.log("输入"+'\n\n\nhttp://localhost:8888/www\n\n\n访问主页桌面!~');
